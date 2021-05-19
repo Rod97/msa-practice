b90b15ce70f51b228f92f09d16630439d5888e09
Rodrigo <rflores456123@gmail.com>
2021-05-19 10:35:46 -0700

    fixing repository

---
+++ DepartmentService/src/main/java/com/rodrigo/department/service/DepartmentService.java
@@ -0,0 +1,27 @@
+package com.rodrigo.department.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigo.department.entity.Department;
+import com.rodrigo.department.repository.DepartmentRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	public Department saveDepartment(Department department) {
+		log.info("Inside saveDepartment method of DepartmentService");
+		return departmentRepository.save(department);
+	}
+
+	public Department findDepartmentById(Long departmentId) {
+		log.info("Inside findDepartmentById method of DepartmentService");
+		return departmentRepository.findByDepartmentId(departmentId);
+	}
+}
